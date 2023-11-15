2013585d9f411566dcca81b4270dc0d924cd37ac
Marisa SUTACHUN <118446519+M4RIZH4T@users.noreply.github.com>
2023-11-15 18:29:19 +0100

    Go go go

--- Assets/Scripts/scrEnnemy.cs
+++ Assets/Scripts/scrEnnemy.cs
@@ -7,17 +7,33 @@ public class Ennemy : MonoBehaviour
     public Player player;
     public float speedMob;
     public bool dead;
+    public float amplitude; 
+    public float frequency;
+    public bool moveSin;
     // Start is called before the first frame update
     void Start()
     {
         //speedMob = 0.2f;
         dead = false;
         player = FindObjectOfType<Player>();
+        amplitude = 25.0f;
+        frequency = 5.0f;
     }
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.down*speedMob * Time.deltaTime;
+        if (moveSin)
+        {
+            float newX = Mathf.Sin(Time.time * frequency) * amplitude;// *Time.deltaTime;
+            transform.position += new Vector3(newX, -speedMob, 0) * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.down * speedMob * Time.deltaTime;
+        }
+        
+
+
         if (transform.position.y < -5.5 && !dead)
         {
             dead = true;
